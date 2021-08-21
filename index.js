ca60249708f7fb8749b317f96b59e9a8d07b9f61
Fabio Vaquera <fabiorvm16500@gmail.com>
2021-08-20 23:23:30 -0400

    commit junto a editar, eliminar, completado, pendiente

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser')
 
 var jsonParser = bodyParser.json()
 //var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
+var surrogateKey = 1;
 var app = express();
 
 var tasks = []
@@ -15,15 +15,84 @@ app.get("/", (req, res, next) => {
 });
 
 app.post("/tasks", jsonParser, (req, res, next) => {
-    req.body.id = tasks.length + 1;
+    req.body.id = surrogateKey++;
     tasks.push(req.body);
-    res.send("OK");
+    res.send("OK!");
+});
+
+//update values, completed tasks, pending tasks
+app.put("/tasks/:taskId", jsonParser, (req, res, next) => {
+    var status =req.query.state;
+    var llegada = true;
+    console.log(status  );
+    if ( status == "completed" || status=="pending"){
+        llegada = false;
+        console.log("si esta aqui");
+    }
+    if (llegada){
+        console.log("bueno no habia nada");
+    if(tasks.length>0){
+        for (var i=0; i<tasks.length; i++){
+                if(tasks[i].id == req.params.taskId ){
+                    tasks[i].title = req.body.title;
+                    tasks[i].detail = req.body.detail;
+                    res.send("UPDATE!!!");
+                    break;
+                }
+        }
+    }
+    else{
+        res.send("Array empty");
+    }
+}
+else{
+    if (status =="completed"){
+        for(var i=0; i<tasks.length; i++){
+            if(tasks[i].id == req.params.taskId ){
+                tasks[i].state = status;
+                res.send("tasks completed!");
+            }
+        }
+    }
+    else{
+        for(var i=0; i<tasks.length; i++){
+            if(tasks[i].id == req.params.taskId){
+                tasks[i].state = status;
+                res.send("tasks pending!");
+            }
+        }
+    }
+    
+}
+    
 });
 
 app.get("/tasks", (req, res, next) => {
     res.json(tasks);
 });
 
+app.get("/tasks/:taskId", (req, res) => {
+    res.send("TaskId is sent to "+ req.params.taskId)
+});
+
 app.listen(3000, () => {
     console.log("Servidor HTTP funcionando");
+});
+
+//delete element 
+app.delete("/tasks/:taskId", jsonParser, (req, res, next) => {
+    if(tasks.length>0){
+        for (var i=0; i<tasks.length; i++){
+                if(tasks[i].id == req.params.taskId ){
+                    tasks.splice(i,1);
+                    res.send("DELETE :(");
+                    break;
+                }
+        }
+    }
+    else{
+        res.send("Array empty");
+    }
+    
+    
 });
