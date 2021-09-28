66b3e0b371bb62adea23777254a5a3112518730c
Ricardo16500 <fabiorvm16500@gmail.com>
2021-09-27 20:42:55 -0400

    terminando el proyecto parte backend

--- index.js
+++ index.js
@@ -36,11 +36,187 @@ var conn = mysql.createConnection({
 app.get("/", (req, res, next) => {
     res.json("{ 'message': 'Tasks server online'}");
 });
+app.get("/tasks", (req, res, next) => {
+    const token = req.headers.authorization;
+    const tknValidate =  validateToken(token);
+    console.log(tknValidate);
+    var userId = null;
+    if(tknValidate){
+        console.log("Authorization correct",tknValidate);
+        userId = tknValidate.sub;
 
+    }else{
+        res.sendStatus(403);
+        return;
+    }
+    const sql = "SELECT username, task_id, title,title,detail,task_status,status "+
+    "FROM task , tk_user  WHERE tk_user.user_id = '"+userId+"' AND  tk_user.user_id = task.user_id AND status = 1";
+    conn.query( sql, 
+        function (err, result) { // CALLBACK HELL -> PROMISES CASCADE HELL -> ASYNC AWAIT
+            if (err) throw err; // Si existe error se para la ejecucion con throe
+            // Si no existe error imprimimos en consola 
+            console.log("Result: " + result);
+            // Retornamos el contendio
+            res.json(result);
+        }
+    );   
+    
+});
 app.post("/tasks", jsonParser, (req, res, next) => {
-    req.body.id = surrogateKey++;
-    tasks.push(req.body);
-    res.send("OK!");
+    //req.body.id = surrogateKey++;
+    //tasks.push(req.body);
+    //res.send("OK!");
+    console.log("AGREGAR NUEVA TAREA");
+    const appData = req.body;
+    console.log(appData);
+   
+    const token = req.headers.authorization;
+    const tknValidate =  validateToken(token);
+    var userId = null;
+    if(tknValidate){
+        console.log("Authorization correct",tknValidate);
+        userId = tknValidate.sub;
+
+    }else{
+        res.sendStatus(403);
+        return;
+    }
+
+    const sql= "INSERT INTO task VALUES (null,'"+appData.title+"','"+appData.detail+"','PENDING','1','"+userId+"' )";
+    conn.query( sql, 
+        function (err, result) { // CALLBACK HELL -> PROMISES CASCADE HELL -> ASYNC AWAIT
+            if (err) throw err; // Si existe error se para la ejecucion con throe
+            // Si no existe error imprimimos en consola 
+            console.log("Result: " + result);
+            // Retornamos el contendio
+            res.json(result);
+        }
+    );   
+});
+//delete element 
+app.delete("/tasks/:taskId", jsonParser, (req, res, next) => {
+    
+    const params = req.params.taskId;
+
+    const token = req.headers.authorization;
+    console.log("DELETE");
+    console.log(token);
+    const tknValidate =  validateToken(token);
+    var userId = null;
+    if(tknValidate){
+        console.log("Authorization correct",tknValidate);
+        userId = tknValidate.sub;
+        const sql = "UPDATE  task set status = 0 WHERE task_id =  '"+params+"'  ";
+        conn.query( sql, 
+            function (err, result) { // CALLBACK HELL -> PROMISES CASCADE HELL -> ASYNC AWAIT
+                if (err) throw err; // Si existe error se para la ejecucion con throe
+                // Si no existe error imprimimos en consola 
+                console.log("Result: " + result);
+                // Retornamos el contendio
+                res.json(result);
+            }
+        );   
+    }
+    else{
+        res.sendStatus(403);
+        return;
+    }
+   
+    
+});
+app.put("/tasks/:taskId", jsonParser, (req, res, next) => {
+    const token = req.headers.authorization;
+    console.log("ACTUALIZAR, COMPLETED, PENDING");
+
+    
+    const params = req.params.taskId;
+    const tknValidate =  validateToken(token)
+    console.log (tknValidate);
+    var status =req.query.task_status;
+    var llegada = true;
+    console.log(status );
+    console.log(params);
+    var userId = null;
+    
+    if ( status == 'COMPLETED' || status=='PENDING'){
+        llegada = false;
+        console.log("si esta aqui");
+    }
+    if (llegada){
+        const appData = req.body;
+    console.log(appData);
+        console.log("bueno no habia nada");
+        if(tknValidate){
+            console.log("Authorization correct",tknValidate);
+            userId = tknValidate.sub;
+    
+        }else{
+            res.sendStatus(403);
+            return;
+        }
+        const sql = "UPDATE  task set title = '" +appData.title + "',detail = '"+ appData.detail+"' WHERE task_id =  '"+params+"' ";
+    conn.query( sql, 
+        function (err, result) { // CALLBACK HELL -> PROMISES CASCADE HELL -> ASYNC AWAIT
+            if (err) throw err; // Si existe error se para la ejecucion con throe
+            // Si no existe error imprimimos en consola 
+            console.log("Result: " + result);
+            // Retornamos el contendio
+            res.json(result);
+        }
+    );
+   
+}
+else{
+    if (status =='COMPLETED'){
+    
+        if(tknValidate){
+            console.log("Authorization correct",tknValidate);
+            userId = tknValidate.sub;
+    
+        }else{
+            res.sendStatus(403);
+            return;
+        }
+        const sql = "UPDATE task set task_status = 'COMPLETED'  WHERE task_id =  '"+params+"'  ";
+    conn.query( sql, 
+        function (err, result) { // CALLBACK HELL -> PROMISES CASCADE HELL -> ASYNC AWAIT
+            if (err) throw err; // Si existe error se para la ejecucion con throe
+            // Si no existe error imprimimos en consola 
+            console.log("Result: " + result);
+            // Retornamos el contendio
+            res.json(result);
+        }
+    );
+    }
+    else{
+        /*for(var i=0; i<tasks.length; i++){
+            if(tasks[i].id == req.params.taskId){
+                tasks[i].state = status;
+                res.send("tasks pending!");
+            }
+        }*/
+        if(tknValidate){
+            console.log("Authorization correct",tknValidate);
+            userId = tknValidate.sub;
+    
+        }else{
+            res.sendStatus(403);
+            return;
+        }
+        const sql = "UPDATE  task set task_status = 'PENDING'  WHERE task_id =  '"+params+"'  ";
+    conn.query( sql, 
+        function (err, result) { // CALLBACK HELL -> PROMISES CASCADE HELL -> ASYNC AWAIT
+            if (err) throw err; // Si existe error se para la ejecucion con throe
+            // Si no existe error imprimimos en consola 
+            console.log("Result: " + result);
+            // Retornamos el contendio
+            res.json(result);
+        }
+    );
+    }
+    
+}
+    
 });
 app.post("/user",jsonParser,(req,res,next)=>{
     const appData = req.body;
@@ -80,64 +256,27 @@ app.post("/auth",jsonParser, (req,res,next)=> {
 function hashPassword(pwd){
     return(sha256.sha256(sha256.sha256(pwd+"trabajo1ingenieriasoftware")+"fabio_vaquera"));
 }
+function validateToken(token){
+    console.log("estamos viendo si el token es valido");
+    try {
+        const jwtValidate = jwt.verify(token,"16052000");
+        return jwtValidate;
+    } catch (error) {
+        return null;
+    }
+}
 function buildToken(userId,username){
     const payload = {
         sub: userId,
         username: username,
         exp: Math.floor(Date.now()/1000)+(60*2)
     };
-    return jwt.sign(payload,"16052000123456789");
+    return jwt.sign(payload,"16052000");
 }
 //update values, completed tasks, pending tasks
-app.put("/tasks/:taskId", jsonParser, (req, res, next) => {
-    var status =req.query.state;
-    var llegada = true;
-    console.log(status  );
-    if ( status == "completed" || status=="pending"){
-        llegada = false;
-        console.log("si esta aqui");
-    }
-    if (llegada){
-        console.log("bueno no habia nada");
-    if(tasks.length>0){
-        for (var i=0; i<tasks.length; i++){
-                if(tasks[i].id == req.params.taskId ){
-                    tasks[i].title = req.body.title;
-                    tasks[i].detail = req.body.detail;
-                    res.send("UPDATE!!!");
-                    break;
-                }
-        }
-    }
-    else{
-        res.send("Array empty");
-    }
-}
-else{
-    if (status =="completed"){
-        for(var i=0; i<tasks.length; i++){
-            if(tasks[i].id == req.params.taskId ){
-                tasks[i].state = status;
-                res.send("tasks completed!");
-            }
-        }
-    }
-    else{
-        for(var i=0; i<tasks.length; i++){
-            if(tasks[i].id == req.params.taskId){
-                tasks[i].state = status;
-                res.send("tasks pending!");
-            }
-        }
-    }
-    
-}
-    
-});
 
-app.get("/tasks", (req, res, next) => {
-    res.json(tasks);
-});
+
+
 
 app.get("/tasks/:taskId", (req, res) => {
     res.send("TaskId is sent to "+ req.params.taskId)
@@ -147,20 +286,3 @@ app.listen(3000, () => {
     console.log("Servidor HTTP funcionando");
 });
 
-//delete element 
-app.delete("/tasks/:taskId", jsonParser, (req, res, next) => {
-    if(tasks.length>0){
-        for (var i=0; i<tasks.length; i++){
-                if(tasks[i].id == req.params.taskId ){
-                    tasks.splice(i,1);
-                    res.send("DELETE :(");
-                    break;
-                }
-        }
-    }
-    else{
-        res.send("Array empty");
-    }
-    
-    
-});
